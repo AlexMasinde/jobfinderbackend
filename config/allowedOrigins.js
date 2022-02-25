cdde4c1cafc110d22b9cbbe6ef6287600922c1cb
Kelvin Kitika <lukkitt@live.co.uk>
2022-02-26 02:59:35 +0300

    updated origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5001/",
-  "https://jobfinderfontend.vercel.app/",
+  "https://jobfinderfontend.vercel.app",
   "https://jobfinderkenya2022.herokuapp.com/",
 ];
 module.exports = allowedOrigins;
