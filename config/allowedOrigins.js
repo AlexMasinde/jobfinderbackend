b69df273d61ef7eae6e6b410740a4183a64cf6b7
Kelvin Kitika <lukkitt@live.co.uk>
2022-02-26 02:01:41 +0300

    added origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,2 +1,6 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5001/"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5001/",
+  "https://jobfinderkenya2022.herokuapp.com/",
+];
 module.exports = allowedOrigins;
